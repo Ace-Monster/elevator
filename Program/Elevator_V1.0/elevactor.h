53f125c8a1ea2bea70d207581f05ef67afca6142
Ace-Monster <36086223+Ace-Monster@users.noreply.github.com>
2018-04-26 17:22:29 +0800

    <style>:<代码规格化>

--- Program/Elevator_V1.0/elevactor.h
+++ Program/Elevator_V1.0/elevactor.h
@@ -1,11 +1,11 @@
 #pragma once
-class QSS {
+class ASK {
 public:
 	int requesttime;//请求时间
 	int waitflt;//起始楼数
 	int tegetflt;//目标楼层
-	int quan = 0;//当前等待顺序，-1代表已上电梯，0代表未发出请求，1代表未上电梯
-	/*bool operator <  (const QSS &p) const
+	int sta = 0;//当前等待顺序，-1代表已上电梯，0代表未发出请求，1代表未上电梯
+	/*bool operator <  (const ASK &p) const
 	{
 		return this->requesttime < p.requesttime;
 	}*/
@@ -17,7 +17,9 @@ public:
 	int systime = 0;//系统时间
 	int onetime = 0;//单次运行时间
 	int sumtime = 0;//已运行总时间
-	int tegettime = 9;//电梯在当前目标楼层停下需要花费的时间
+	int tegettime = 1;//电梯在当前目标楼层停下需要花费的时间
+	Time(int m){ tegettime = m; }
+	~Time() { }
 };
 
 class Peo {
@@ -35,7 +37,7 @@ public:
 	int tegetflt = 3;//电梯当前目标楼层
 };
 
-class DT {
+class ELE {
 public:
 	Time times;
 	Peo peo;
@@ -44,96 +46,98 @@ public:
 	{
 		return times.onetime * peo.peo_size();
 	}
+	ELE(int m) : times(m) { }
+	~ELE() { }
 	
 };//电梯状态
 
-int plan(DT *tempd, int i, QSS tempq[], int wt[], bool tflt[], int n)//下一层判断前临时改变电梯参数
+int plan(ELE *tempd, int i, ASK tempAsk[], int wait_peo[], bool wait_flt[], int n)//下一层判断前临时改变电梯参数
 {
 	tempd->times.onetime = abs(tempd->flt.flt - i);
 	tempd->times.systime += tempd->times.onetime;
 	tempd->times.sumtime += tempd->times.onetime;
 	tempd->flt.flt = i;
-	tflt[tempd->flt.flt] = 0;
+	wait_flt[tempd->flt.flt] = 0;
 	int t = tempd->onecenum();
 	for (int k = 0;k < n;k++)
 	{
-		if (tempq[k].waitflt == i && tempq[k].quan == 1)
-			tempq[k].quan = -1, tempd->peo.peo[1]--, tempd->peo.peo[0]++, tflt[tempq[k].tegetflt] = 1;
-		else if (tempq[k].tegetflt == i && tempq[k].quan == -1)
-			tempq[k].quan = -2, tempd->peo.peo[0]--;
+		if (tempAsk[k].waitflt == i && tempAsk[k].sta == 1)
+			tempAsk[k].sta = -1, tempd->peo.peo[1]--, tempd->peo.peo[0]++, wait_flt[tempAsk[k].tegetflt] = 1;
+		else if (tempAsk[k].tegetflt == i && tempAsk[k].sta == -1)
+			tempAsk[k].sta = -2, tempd->peo.peo[0]--;
 	}
-	wt[i] = 0;
+	wait_peo[i] = 0;
 	tempd->times.systime++, tempd->times.sumtime++, t += tempd->peo.peo_size();
 	return t;
 }
 
-int yc(DT tempd, QSS tq[], int wti[], bool tf[], int j, int n)//ycz调用的，第二及之后的判断，返回电梯运行时间
+int yc(ELE tempEle, ASK tAsk[], int wait_peo_i[], bool wait_flt_i[], int j, int n)//ycz调用的，第二及之后的判断，返回电梯运行时间
 {
-	bool tflt[11];
-	memcpy(tflt, tf, sizeof(tflt));
-	QSS *tempq = new QSS[n];int wt[11];
-	memcpy(tempq, tq, sizeof(tempq));
-	memcpy(wt, wti, sizeof(wt));
-	int t_numi = plan(&tempd, j, tempq, wt, tflt, n);
+	bool wait_flt[11];
+	memcpy(wait_flt, wait_flt_i, sizeof(wait_flt));
+	ASK *tempAsk = new ASK[n];int wait_peo[11];
+	memcpy(tempAsk, tAsk, sizeof(tempAsk));
+	memcpy(wait_peo, wait_peo_i, sizeof(wait_peo));
+	int t_numi = plan(&tempEle, j, tempAsk, wait_peo, wait_flt, n);
 	int tt_num = 1000000;
 	for (int i = 1;i <= 3;i++)
 	{
-		if (!tflt[i])
+		if (!wait_flt[i])
 			continue;
 		int t_num = t_numi;
-		t_num += yc(tempd, tempq, wt, tflt, i, n);//递归
+		t_num += yc(tempEle, tempAsk, wait_peo, wait_flt, i, n);//递归
 		if (tt_num > t_num)
 			tt_num = t_num;
 	}
 	if (tt_num == 1000000)
 		return t_numi;
-	delete tempq;
+	delete tempAsk;
 	return tt_num;//返回估计时间
 }
 
-DT ycz(DT tempd, QSS tempq[], int wt[], bool tflt[], int n)//第一层判断，直接决定电梯的下一步运行目标
+ELE ycz(ELE tempEle, ASK tempAsk[], int wait_peo[], bool wait_flt[], int n)//第一层判断，直接决定电梯的下一步运行目标
 {
 	int tt_num = 1000;
 	int tempflt = 0;
 	for (int i = 1;i <= 3;i++)
 	{
-		if (!tflt[i])
+		if (!wait_flt[i])
 			continue;
-		int t_num = yc(tempd, tempq, wt, tflt, i, n);
+		int t_num = yc(tempEle, tempAsk, wait_peo, wait_flt, i, n);
 		if (tt_num > t_num)
 		{
 			tempflt = i;
 			tt_num = t_num;
-			tempd.flt.tegetflt = i;
-			tempd.times.tegettime = abs(tempd.flt.flt - tempd.flt.tegetflt);
+			tempEle.flt.tegetflt = i;
+			tempEle.times.tegettime = abs(tempEle.flt.flt - tempEle.flt.tegetflt);
 		}
 	}
-	return tempd;//返回电梯参数
+	return tempEle;//返回电梯参数
 }
 
-void stopmassage(DT &d, QSS q[], int wt[], bool tflt[], int n)//停下后的状态调整
+void stopmassage(ELE &Ele, ASK Ask[], int wait_peo[], bool wait_flt[], int n)//停下后的状态调整
 {
 	
-	d.times.num += d.peo.peo_size() * d.times.tegettime;
-	d.times.systime += d.times.tegettime;
-	d.times.sumtime += d.times.tegettime;
-	d.flt.flt = d.flt.tegetflt;
-	tflt[d.flt.flt] = 0;
-	int temp_peo = d.peo.peo_size(), temp_peo2 = d.peo.peo[1];
-	d.peo.peo[0] += wt[d.flt.flt], d.peo.peo[1] -= wt[d.flt.flt], wt[d.flt.flt] = 0;
+	Ele.times.num += Ele.peo.peo_size() * Ele.times.tegettime;
+	Ele.times.systime += Ele.times.tegettime;
+	Ele.times.sumtime += Ele.times.tegettime;
+	Ele.flt.flt = Ele.flt.tegetflt;
+	wait_flt[Ele.flt.flt] = 0;
+	int temp_peo = Ele.peo.peo_size(), temp_peo2 = Ele.peo.peo[1];
+	Ele.peo.peo[0] += wait_peo[Ele.flt.flt], Ele.peo.peo[1] -= wait_peo[Ele.flt.flt], wait_peo[Ele.flt.flt] = 0;
 	for (int k = 0;k < n;k++)
 	{
-		if (q[k].waitflt == d.flt.flt && q[k].quan == 1)
-			q[k].quan = -1, tflt[q[k].tegetflt] = 1;
-		else if (q[k].tegetflt == d.flt.flt && q[k].quan == -1)
-			q[k].quan = -2, d.peo.peo[0]--;
+		if (Ask[k].waitflt == Ele.flt.flt && Ask[k].sta == 1)
+			Ask[k].sta = -1, wait_flt[Ask[k].tegetflt] = 1;
+		else if (Ask[k].tegetflt == Ele.flt.flt && Ask[k].sta == -1)
+			Ask[k].sta = -2, Ele.peo.peo[0]--;
 	}
 	/*
 	printf("第%d秒，电梯在%d停靠，%d人下电梯，%d人上电梯，电梯内%d人\n",
-	d.systime, d.flt, temp_peo - d.peo_size(), temp_peo2 - d.peo[2], d.peo_size() - d.peo[2]);
+	Ele.systime, Ele.flt, temp_peo - Ele.peo_size(), temp_peo2 - Ele.peo[2], Ele.peo_size() - Ele.peo[2]);
 	*///注释部分用于调试，输出每次停下时的人员变化
-	d.times.systime++, d.times.sumtime++;
-	d.times.num += d.peo.peo_size();
-	std::cout << d.times.systime << ' ' << d.flt.flt << std::endl;
-	d = ycz(d, q, wt, tflt, n);
+	Ele.times.systime++, Ele.times.sumtime++;
+	Ele.times.num += Ele.peo.peo_size();
+	std::cout << Ele.times.systime << ' ' << Ele.flt.flt << std::endl;
+	Ele = ycz(Ele, Ask, wait_peo, wait_flt, n);
 }
--- Program/Elevator_V1.0/main.cpp
+++ Program/Elevator_V1.0/main.cpp
@@ -12,39 +12,42 @@ int main()
 	int n;//请求人数
 	int m = 3;//楼层数量
 	cin >> n;
-	QSS *q = new QSS[n];
-	DT d;
+	ASK *Ask = new ASK[n];
+	ELE Ele(m);
 	for (int i = 0; i < n; i++)
-	cin >> q[i].requesttime >> q[i].waitflt >> q[i].tegetflt;
+	cin >> Ask[i].requesttime >> Ask[i].waitflt >> Ask[i].tegetflt;
 	//sort(q, q + 5);//按时间排序
-	int *wt = new int[m];
-	bool *tflt = new bool[m];
+	int *wait_peo = new int[m];
+	bool *wait_flt = new bool[m];
+	memset(wait_peo, 0, sizeof(wait_peo));
+	memset(wait_flt, 0, sizeof(wait_flt));
 	for (int i = 0;i < n;i++)//未收全请求前，没收到一次请求就判断一次
 	{
-		if (d.peo.peo_size() == 0)//更新系统时间
-			d.times.systime = q[i].requesttime;
-		while ((d.times.systime + d.times.tegettime) < q[i].requesttime)//该乘客请求未发出时运行
+		if (Ele.peo.peo_size() == 0)//更新系统时间
+			Ele.times.systime = Ask[i].requesttime;
+		while ((Ele.times.systime + Ele.times.tegettime) < Ask[i].requesttime)//该乘客请求未发出时运行
 		{
-			if (d.peo.peo_size() == 0)
-				d.times.systime = q[i].requesttime;
+			if (Ele.peo.peo_size() == 0)
+				Ele.times.systime = Ask[i].requesttime;
 			else
-				stopmassage(d, q, wt, tflt, n);
+				stopmassage(Ele, Ask, wait_peo, wait_flt, n);
 		}
-		d.times.num += (q[i].requesttime - d.times.systime) * d.peo.peo_size();
-		d.times.sumtime += q[i].requesttime - d.times.systime;
-		if (d.flt.flt > d.flt.tegetflt)
-			d.flt.flt -= q[i].requesttime - d.times.systime;
+		Ele.times.num += (Ask[i].requesttime - Ele.times.systime) * Ele.peo.peo_size();
+		Ele.times.sumtime += Ask[i].requesttime - Ele.times.systime;
+		if (Ele.flt.flt > Ele.flt.tegetflt)
+			Ele.flt.flt -= Ask[i].requesttime - Ele.times.systime;
 		else
-			d.flt.flt += q[i].requesttime - d.times.systime;
-		d.times.systime = q[i].requesttime;
-		wt[q[i].waitflt]++;
-		tflt[q[i].waitflt] = 1;
-		d.peo.peo[1]++;
-		q[i].quan = 1;
-		d = ycz(d, q, wt, tflt, n);
+			Ele.flt.flt += Ask[i].requesttime - Ele.times.systime;
+		Ele.times.systime = Ask[i].requesttime;
+		wait_peo[Ask[i].waitflt]++;
+		wait_flt[Ask[i].waitflt] = 1;
+		Ele.peo.peo[1]++;
+		Ask[i].sta = 1;
+		Ele = ycz(Ele, Ask, wait_peo, wait_flt, n);
 	}
-	while (d.peo.peo_size() != 0)//直到所有人全部完成请求
-		stopmassage(d, q, wt, tflt, n);
-	//cout << d.num;
+	while (Ele.peo.peo_size() != 0)//直到所有人全部完成请求
+		stopmassage(Ele, Ask, wait_peo, wait_flt, n);
+	//cout << Ele.num;
+	//system("pause");
 	return 0;
 }
